ef24680fe939ba9008addad6baa29ff19c4f689b
ppzhang123 <939223493@qq.com>
2023-03-13 00:11:07 +0800

    feat: 提交构建页面

---
+++ .next/server/chunks/683.js
@@ -0,0 +1,541 @@
+exports.id = 683;
+exports.ids = [683];
+exports.modules = {
+
+/***/ 474:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"w1200": "Footer_w1200__U2etD",
+	"footer": "Footer_footer__BcpJO",
+	"footerS": "Footer_footerS__lavM6",
+	"wbline": "Footer_wbline__b0HS1",
+	"copyright": "Footer_copyright__xvwbw"
+};
+
+
+/***/ }),
+
+/***/ 9328:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"headTop": "Header_headTop__8TLAB",
+	"flexTop": "Header_flexTop__USvZv",
+	"headLogo": "Header_headLogo__M5sFr",
+	"nav": "Header_nav__7PnJq",
+	"navMenu": "Header_navMenu__pmlQC",
+	"w1200": "Header_w1200__xhnfk"
+};
+
+
+/***/ }),
+
+/***/ 4683:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "Z": () => (/* binding */ Layout)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(6689);
+// EXTERNAL MODULE: ./node_modules/.pnpm/registry.npmmirror.com+next@13.2.3_wiv434v7erz4aedd5whhdwmpv4/node_modules/next/image.js
+var next_image = __webpack_require__(1599);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+// EXTERNAL MODULE: ./components/Footer.module.scss
+var Footer_module = __webpack_require__(474);
+var Footer_module_default = /*#__PURE__*/__webpack_require__.n(Footer_module);
+;// CONCATENATED MODULE: ./components/Footer.js
+
+
+
+
+function Footer() {
+    return /*#__PURE__*/ jsx_runtime_.jsx("div", {
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+            className: (Footer_module_default()).footer,
+            children: [
+                /*#__PURE__*/ (0,jsx_runtime_.jsxs)("ul", {
+                    className: (Footer_module_default()).footerS,
+                    children: [
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("li", {
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+                                    children: "公司简介"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/list-2-1.html",
+                                    title: "公司介绍",
+                                    children: "公司介绍"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/list-3-1.html",
+                                    title: "企业文化",
+                                    children: "企业文化"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/list-4-1.html",
+                                    title: "技术团队",
+                                    children: "技术团队"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/list-24-1.html",
+                                    title: "主营业务",
+                                    children: "主营业务"
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("li", {
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+                                    children: "资讯交流"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/index.php?m=content&c=index&a=lists&catid=5",
+                                    title: "新闻中心",
+                                    children: "新闻中心"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/list-6-1.html",
+                                    title: "政策法规",
+                                    children: "政策法规"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/list-14-1.html",
+                                    title: "技术交流",
+                                    children: "技术交流"
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("li", {
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+                                    children: "其他"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "",
+                                    title: "",
+                                    children: "新闻中心"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/hpgs/",
+                                    title: "信息公示",
+                                    children: "政策法规"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/list-16-1.html",
+                                    title: "合作机构",
+                                    children: "技术交流"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "http://www.shuiluyuan.cn/list-17-1.html",
+                                    title: "联系我们",
+                                    children: "新闻中心"
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("li", {
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+                                    children: "扫描关注"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                                    src: "/weixin.jpg",
+                                    alt: "Vercel Logo",
+                                    className: (Footer_module_default()).vercelLogo,
+                                    width: 72,
+                                    height: 72,
+                                    priority: true
+                                })
+                            ]
+                        })
+                    ]
+                }),
+                /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                    className: (Footer_module_default()).wbline
+                }),
+                /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                    className: (Footer_module_default()).copyright,
+                    children: [
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("p", {
+                            children: [
+                                "版权所有：贵州水陆源生态环境咨询有限公司    ",
+                                /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                                    href: "https://beian.miit.gov.cn",
+                                    target: "_blank",
+                                    style: {
+                                        color: "#0067BE"
+                                    },
+                                    children: "黔ICP备18006237号-1"
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                            children: "地址:贵州省贵阳市观山湖区麒龙商务港1栋11楼"
+                        })
+                    ]
+                })
+            ]
+        })
+    });
+}
+
+// EXTERNAL MODULE: external "antd"
+var external_antd_ = __webpack_require__(5725);
+// EXTERNAL MODULE: ./components/Header.module.scss
+var Header_module = __webpack_require__(9328);
+var Header_module_default = /*#__PURE__*/__webpack_require__.n(Header_module);
+// EXTERNAL MODULE: ./node_modules/.pnpm/registry.npmmirror.com+next@13.2.3_wiv434v7erz4aedd5whhdwmpv4/node_modules/next/link.js
+var next_link = __webpack_require__(4867);
+var link_default = /*#__PURE__*/__webpack_require__.n(next_link);
+;// CONCATENATED MODULE: ./components/Header.js
+
+
+
+
+
+
+function Header() {
+    const items = [
+        {
+            label: /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                href: "/",
+                children: "首页"
+            }),
+            key: "Home"
+        },
+        {
+            //   label: '公司简介',
+            label: /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                href: "/Introduction",
+                children: "公司简介"
+            }),
+            key: "Introduction"
+        },
+        {
+            label: "新闻中心",
+            key: "NewMenu",
+            theme: "light",
+            children: [
+                {
+                    type: "group",
+                    label: "Item 1",
+                    children: [
+                        {
+                            label: "Option 1",
+                            key: "setting:1"
+                        },
+                        {
+                            label: "Option 2",
+                            key: "setting:2"
+                        }
+                    ]
+                },
+                {
+                    type: "group",
+                    label: "Item 2",
+                    children: [
+                        {
+                            label: "Option 3",
+                            key: "setting:3"
+                        },
+                        {
+                            label: "Option 4",
+                            key: "setting:4"
+                        }
+                    ]
+                }
+            ]
+        },
+        // {
+        //   label: '业务范围',
+        //   key: 'BusinessMenu',
+        //   children: [
+        //     {
+        //       type: 'group',
+        //       label: 'Item 1',
+        //       children: [
+        //         {
+        //           label: 'Option 1',
+        //           key: 'setting:1',
+        //         },
+        //         {
+        //           label: 'Option 2',
+        //           key: 'setting:2',
+        //         },
+        //       ],
+        //     },
+        //     {
+        //       type: 'group',
+        //       label: 'Item 2',
+        //       children: [
+        //         {
+        //           label: 'Option 3',
+        //           key: 'setting:3',
+        //         },
+        //         {
+        //           label: 'Option 4',
+        //           key: 'setting:4',
+        //         },
+        //       ],
+        //     },
+        //   ],
+        // },
+        // {
+        //   label: '成功案例',
+        //   key: 'SubMenu',
+        //   children: [
+        //     {
+        //       type: 'group',
+        //       label: 'Item 1',
+        //       children: [
+        //         {
+        //           label: 'Option 1',
+        //           key: 'setting:1',
+        //         },
+        //         {
+        //           label: 'Option 2',
+        //           key: 'setting:2',
+        //         },
+        //       ],
+        //     },
+        //     {
+        //       type: 'group',
+        //       label: 'Item 2',
+        //       children: [
+        //         {
+        //           label: 'Option 3',
+        //           key: 'setting:3',
+        //         },
+        //         {
+        //           label: 'Option 4',
+        //           key: 'setting:4',
+        //         },
+        //       ],
+        //     },
+        //   ],
+        // },
+        // {
+        //   label: '政策法规',
+        //   key: 'PoliciesMenu',
+        //   children: [
+        //     {
+        //       type: 'group',
+        //       label: 'Item 1',
+        //       children: [
+        //         {
+        //           label: 'Option 1',
+        //           key: 'setting:1',
+        //         },
+        //         {
+        //           label: 'Option 2',
+        //           key: 'setting:2',
+        //         },
+        //       ],
+        //     },
+        //     {
+        //       type: 'group',
+        //       label: 'Item 2',
+        //       children: [
+        //         {
+        //           label: 'Option 3',
+        //           key: 'setting:3',
+        //         },
+        //         {
+        //           label: 'Option 4',
+        //           key: 'setting:4',
+        //         },
+        //       ],
+        //     },
+        //   ],
+        // },
+        // {
+        //   label: '下载中心',
+        //   key: 'DownloadMenu',
+        //   children: [
+        //     {
+        //       type: 'group',
+        //       label: 'Item 1',
+        //       children: [
+        //         {
+        //           label: 'Option 1',
+        //           key: 'setting:1',
+        //         },
+        //         {
+        //           label: 'Option 2',
+        //           key: 'setting:2',
+        //         },
+        //       ],
+        //     },
+        //     {
+        //       type: 'group',
+        //       label: 'Item 2',
+        //       children: [
+        //         {
+        //           label: 'Option 3',
+        //           key: 'setting:3',
+        //         },
+        //         {
+        //           label: 'Option 4',
+        //           key: 'setting:4',
+        //         },
+        //       ],
+        //     },
+        //   ],
+        // },
+        // {
+        //   label: '信息公示',
+        //   key: 'PublicityMenu',
+        //   children: [
+        //     {
+        //       type: 'group',
+        //       label: 'Item 1',
+        //       children: [
+        //         {
+        //           label: 'Option 1',
+        //           key: 'setting:1',
+        //         },
+        //         {
+        //           label: 'Option 2',
+        //           key: 'setting:2',
+        //         },
+        //       ],
+        //     },
+        //     {
+        //       type: 'group',
+        //       label: 'Item 2',
+        //       children: [
+        //         {
+        //           label: 'Option 3',
+        //           key: 'setting:3',
+        //         },
+        //         {
+        //           label: 'Option 4',
+        //           key: 'setting:4',
+        //         },
+        //       ],
+        //     },
+        //   ],
+        // },
+        {
+            label: /*#__PURE__*/ jsx_runtime_.jsx("a", {
+                href: "https://ant.design",
+                target: "_blank",
+                rel: "noopener noreferrer",
+                children: "联系我们"
+            }),
+            key: "Contact"
+        }
+    ];
+    const [current, setCurrent] = (0,external_react_.useState)("Home");
+    const onClick = (e)=>{
+        console.log("click ", e);
+        setCurrent(e.key);
+    };
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: (Header_module_default()).headHome,
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (Header_module_default()).headTop,
+                children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                    className: (Header_module_default()).flexTop,
+                    children: [
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx("i", {
+                                    className: "iconfont  icon-dianhua1"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                                    children: "18685417929 "
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("i", {
+                                    className: "iconfont  icon-youxiang"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                                    children: "1129422384@qq.com"
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                            children: [
+                                /*#__PURE__*/ jsx_runtime_.jsx("i", {
+                                    className: "iconfont  icon-shouye2"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                                    children: "设为首页 "
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("i", {
+                                    className: "iconfont  icon-shoucang"
+                                }),
+                                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                                    children: "加入收藏"
+                                })
+                            ]
+                        })
+                    ]
+                })
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (Header_module_default()).headLogo,
+                children: /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                    className: (Header_module_default()).w1200,
+                    children: /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                        src: "/20190715033031884.png",
+                        alt: "Vercel Logo",
+                        className: (Header_module_default()).vercelLogo,
+                        width: 500,
+                        height: 76,
+                        priority: true
+                    })
+                })
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (Header_module_default()).nav,
+                children: /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                    className: (Header_module_default()).w1200,
+                    children: /*#__PURE__*/ jsx_runtime_.jsx(external_antd_.Menu, {
+                        className: (Header_module_default()).navMenu,
+                        onClick: onClick,
+                        selectedKeys: [
+                            current
+                        ],
+                        mode: "horizontal",
+                        items: items,
+                        theme: "dark"
+                    })
+                })
+            })
+        ]
+    });
+}
+
+;// CONCATENATED MODULE: ./components/layout.js
+
+
+
+
+function Layout({ children  }) {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(Header, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx("main", {
+                children: children
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Footer, {})
+        ]
+    });
+}
+
+
+/***/ })
+
+};
+;
